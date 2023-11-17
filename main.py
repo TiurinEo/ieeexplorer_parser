9cf3c715ac91228d70da74ce8ed7e024a8bec583
Egor <tyurin.egor21@gmail.com>
2023-11-17 20:38:33 +0300

    added async translation + progressbar

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 import pandas as pd
 import json
-from tqdm import tqdm
-tqdm.pandas()
 import pickle
-from googletrans import Translator
 import aiohttp
 import asyncio
 import time
 import typer
 from functools import wraps
-app= typer.Typer()
+from async_google_trans_new import AsyncTranslator
+from tqdm.asyncio import tqdm_asyncio
+
+app = typer.Typer()
+
 
 async def fetch_articles(session, url, page, query):
-    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0',"Origin":
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0', "Origin":
         'https://ieeexplore.ieee.org'}
     data = {
         "queryText": f"{query}",
@@ -23,29 +24,32 @@ async def fetch_articles(session, url, page, query):
         "matchPubs": True,
         "pageNumber": str(page)
     }
-    async with session.post(url, json=data,headers=headers) as response:
+    async with session.post(url, json=data, headers=headers) as response:
         return await response.json()
+
+
 async def parseArctilcesNumbers(pages, query):
     articles = []
     url = 'https://ieeexplore.ieee.org/rest/search/'
 
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_articles(session, url, page, query) for page in range(pages)]
-        results = await asyncio.gather(*tasks)
+        results = await tqdm_asyncio.gather(*tasks, desc='Parsing article ids', unit='page')
         for res in results:
             for record in res.get('records', []):
                 articles.append(record.get('articleNumber'))
 
     return articles
 
-async def fetch_article(session,id):
+
+async def fetch_article(session, id):
     try:
         async with session.get(f'https://ieeexplore.ieee.org/document/{int(id)}', headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0'}) as response:
-            response_text= await response.text()
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0'}) as response:
+            response_text = await response.text()
             xpl_index_start = response_text.index('xplGlobal.document.metadata')
             xpl_index_end = response_text.index('"};', xpl_index_start)
-            data = response_text[xpl_index_start:xpl_index_end+2]  # Including the trailing '};'
+            data = response_text[xpl_index_start:xpl_index_end + 2]  # Including the trailing '};'
             data = data[data.index('=') + 1:]
             return json.loads(data)
     except KeyError:
@@ -53,13 +57,15 @@ async def fetch_article(session,id):
     except ValueError:
         return {f'{id}': 'failed to parse'}
 
+
 async def parse_ids_to_dic(ids):
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_article(session, id) for id in ids]
-        results = await asyncio.gather(*tasks)
+        results = await tqdm_asyncio.gather(*tasks, desc='Parsing articles', unit='article')
         return results
 
-def fill_dataframe(d,id=True,title=True,abstract=True,authors=True,publicationDate=True,year_only=True):
+
+def fill_dataframe(d, id=True, title=True, abstract=True, authors=True, publicationDate=True, year_only=True):
     df = pd.json_normalize(d)
     columns_to_get = []
     if id:
@@ -77,16 +83,19 @@ def fill_dataframe(d,id=True,title=True,abstract=True,authors=True,publicationDa
             df['publicationDate'] = pd.DatetimeIndex(df['publicationDate']).year.astype('Int64')
     df = df[columns_to_get]
     return df
+
+
 def format_date(text):
-    date_formats=('%d %B %Y','%B %Y', '%Y')
+    date_formats = ('%d %B %Y', '%B %Y', '%Y')
     for fmt in date_formats:
         try:
-            return datetime.strptime(text,fmt)
+            return datetime.strptime(text, fmt)
         except ValueError:
             pass
         except TypeError:
             pass
     return None
+
 
 def coro(f):
     @wraps(f)
@@ -95,25 +104,28 @@ def coro(f):
 
     return wrapper
 
+
 @app.command()
 @coro
-async def parse(number_of_pages: int,query:str,
-          output:str, tranlsate: bool = True, id:bool=True,publicationDate:bool=True,year_only:bool=True,
-                title:bool=True,abstract:bool=True,authors:bool=True):
+async def parse(number_of_pages: int, query: str,
+                output: str, tranlsate: bool = True, id: bool = True, publicationDate: bool = True,
+                year_only: bool = True,
+                title: bool = True, abstract: bool = True, authors: bool = True):
     start_time = time.time()
-    print(f'Parsing {number_of_pages} pages of articles,query is {query}, saving to {output}, translating to rus is {tranlsate}')
+    print(
+        f'Parsing {number_of_pages} pages of articles,query is {query}, saving to {output}, translating to rus is {tranlsate}')
     articles = await parseArctilcesNumbers(number_of_pages, query)
     res = await parse_ids_to_dic(articles)
-    df = fill_dataframe(res,id,title,abstract,authors,publicationDate,year_only)
+    df = fill_dataframe(res, id, title, abstract, authors, publicationDate, year_only)
     if tranlsate:
-        trans = Translator()
+        trans = AsyncTranslator()
         if title:
-            print('\nTranslating titles')
+            df['title'] = await tqdm_asyncio.gather(*[trans.translate(title, 'ru') for title in df['title']],
+                                                    desc='Translating titles', unit='title')
 
-            df['title'] = df['title'].progress_apply(lambda x: trans.translate(x, src='en', dest='ru').text)
         if abstract:
-            print('\nTranslating abstracts')
-            df['abstract'] = df['abstract'].progress_apply(lambda x: trans.translate(x, src='en', dest='ru').text)
+            df['abstract'] = await tqdm_asyncio.gather(*[trans.translate(title, 'ru') for title in df['abstract']],
+                                                       desc='Translating abstracts', unit='abtract')
 
     if output:
         try:
@@ -123,12 +135,9 @@ async def parse(number_of_pages: int,query:str,
             print('No such directory')
     print("--- %s seconds ---" % round((time.time() - start_time), 2))
     return df
+
+
 pd.set_option('display.max_columns', 500)
 
-
-def main():
+if __name__ == "__main__":
     app()
-
-
-if __name__=="__main__":
-    main()
