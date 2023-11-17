2d49a78ec7d4507226b25e793ee940321327a97b
Egor <tyurin.egor21@gmail.com>
2023-11-17 15:20:29 +0300

    error fixes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 import pandas as pd
 import requests
@@ -7,15 +8,14 @@ tqdm.pandas()
 import pickle
 from googletrans import Translator
 from icecream import ic
-referer_complex='https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=Text%20Mining,%20Clustering,%20Classification,%20Machine%20Learning'
-def parseArctilcesNumbers(pages):
+def parseArctilcesNumbers(pages,query):
     articles = []
     #in page there is 25 docs
     for j in tqdm(range(0,pages),desc='Parsing article ids',unit='page'):
         res = requests.post(url='https://ieeexplore.ieee.org/rest/search/', headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0',
-            'Referer': referer_complex},
-                            json={"queryText": "Text Mining, Clustering, Classification, Machine Learning", "highlight": True, "returnFacets": ["ALL"],
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0',"Origin":
+        'https://ieeexplore.ieee.org'},
+                            json={"queryText": f"{query}", "highlight": True, "returnFacets": ["ALL"],
                                   "returnType": "SEARCH", "matchPubs": True, "pageNumber":str(j)})
         res_json=json.loads(res.text)
         for i in range(len(res_json['records'])):
@@ -65,30 +65,52 @@ def format_date(text):
             return datetime.strptime(text,fmt)
         except ValueError:
             pass
+        except TypeError:
+            pass
     return None
 
-def parse(number_of_pages,
+def parse(number_of_pages=1,
           save=True,tranlsate_to_rus=False,id=True,title=True,
           abstract=True,authors=True,publicationDate=True,year_only=True):
-
-    all_ids = parseArctilcesNumbers(number_of_pages)
+    while True:
+        try:
+            query = input('Type in topic ("exit" to escape): ')
+            if query == 'exit':
+                break
+        except OSError:
+            print('error')
+        else:
+            break
+    all_ids = parseArctilcesNumbers(number_of_pages,query)
     d = parse_ids_to_dic(all_ids)
-
     df = fill_dataframe(d,id,title,abstract,authors,publicationDate,year_only)
     if tranlsate_to_rus:
         trans = Translator()
         if title:
-            print('\nПеревожу названия статей')
+            print('\nTranslating titles')
             df['title'] = df['title'].progress_apply(lambda x: trans.translate(x, src='en', dest='ru').text)
         if abstract:
-            print('\nПеревожу аннотации статей')
+            print('\nTranslating abstracts')
             df['abstract'] = df['abstract'].progress_apply(lambda x: trans.translate(x, src='en', dest='ru').text)
 
     if save:
-        path=input("Введите путь для сохранения датафрейма (без расширения .pickle): ")
-        with open(path+'.pickle', 'wb') as f:
-            pickle.dump(df, f)
+        while True:
+            try:
+                path = input("Path to save file (w/o .pickle): ")
+                if path == 'exit':
+                    break
+                with open(path + '.pickle', 'wb') as f:
+                    pickle.dump(df, f)
+                    break  # добавляем break, чтобы выйти из цикла после успешного сохранения
+            except OSError:
+                choice = input(
+                    'Non existing path. Type in "exit" to escape, '
+                    'or press Enter to type in path again: ')
+                if choice == 'exit':
+                    break
+                continue
+
     return df
 
-df=parse(1,save=False)
-
+df=parse(5,save=False)
+ic(df)
