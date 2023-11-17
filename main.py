3cc92ca94a35ee21b6e3fbb824ab2d7d96ac63cc
Egor <tyurin.egor21@gmail.com>
2023-11-17 15:24:25 +0300

    async parser with aiohttp

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import sys
 from datetime import datetime
 import pandas as pd
-import requests
+
 import json
 from tqdm import tqdm
 tqdm.pandas()
 import pickle
 from googletrans import Translator
 from icecream import ic
-def parseArctilcesNumbers(pages,query):
+import aiohttp
+import asyncio
+import time
+
+"""def parseArctilcesNumbers(pages,query):
     articles = []
     #in page there is 25 docs
     for j in tqdm(range(0,pages),desc='Parsing article ids',unit='page'):
@@ -20,28 +23,56 @@ def parseArctilcesNumbers(pages,query):
         res_json=json.loads(res.text)
         for i in range(len(res_json['records'])):
             articles.append(res_json['records'][i]['articleNumber'])
+    return articles"""
+
+
+async def fetch_articles(session, url, page, query):
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0',"Origin":
+        'https://ieeexplore.ieee.org'}
+    data = {
+        "queryText": f"{query}",
+        "highlight": True,
+        "returnFacets": ["ALL"],
+        "returnType": "SEARCH",
+        "matchPubs": True,
+        "pageNumber": str(page)
+    }
+    async with session.post(url, json=data,headers=headers) as response:
+        return await response.json()
+async def parseArctilcesNumbers(pages, query):
+    articles = []
+    url = 'https://ieeexplore.ieee.org/rest/search/'
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_articles(session, url, page, query) for page in range(pages)]
+        results = await asyncio.gather(*tasks)
+        for res in results:
+            for record in res.get('records', []):
+                articles.append(record.get('articleNumber'))
+
     return articles
-def parse_ids_to_dic(ids):
-    data=[]
-    for id in tqdm(ids,desc='Parsing articles',unit='article'):
-        try:
-            arctile = requests.get(url=f'https://ieeexplore.ieee.org/document/{int(id)}', headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0'})
-            #конечно смешно, но обычный парс не работает, вроде не хочет заходить за какой то там элемент,
-            #не помню как называется
-            xpl_index_start=arctile.text.index('xplGlobal.document.metadata')
-            xpl_index_end=arctile.text.index('"};',xpl_index_start)
-            str=''
-            for i in range(xpl_index_start,xpl_index_end):
-                str+=arctile.text[i]
-            str = str[str.index('=') + 1:]+'"}'
-            dic=json.loads(str)
-            data.append(dic)
-        except KeyError:
-            data.append({f'{id}':'failed to parse'})
-    return data
+
+async def fetch_article(session,id):
+    try:
+        async with session.get(f'https://ieeexplore.ieee.org/document/{int(id)}', headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/76.0'}) as response:
+            response_text= await response.text()
+            xpl_index_start = response_text.index('xplGlobal.document.metadata')
+            xpl_index_end = response_text.index('"};', xpl_index_start)
+            data = response_text[xpl_index_start:xpl_index_end+2]  # Including the trailing '};'
+            data = data[data.index('=') + 1:]
+            return json.loads(data)
+    except KeyError:
+        return {f'{id}': 'failed to parse'}
+
+async def parse_ids_to_dic(ids):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_article(session, id) for id in ids]
+        results = await asyncio.gather(*tasks)
+        return results
+
 def fill_dataframe(d,id=True,title=True,abstract=True,authors=True,publicationDate=True,year_only=True):
-    df = pd.DataFrame(d)
+    df = pd.json_normalize(d)
     columns_to_get = []
     if id:
         columns_to_get.append('articleId')
@@ -69,21 +100,23 @@ def format_date(text):
             pass
     return None
 
-def parse(number_of_pages=1,
+async def parse(number_of_pages=1,
           save=True,tranlsate_to_rus=False,id=True,title=True,
           abstract=True,authors=True,publicationDate=True,year_only=True):
+
     while True:
         try:
             query = input('Type in topic ("exit" to escape): ')
             if query == 'exit':
                 break
-        except OSError:
+        except ValueError:
             print('error')
         else:
             break
-    all_ids = parseArctilcesNumbers(number_of_pages,query)
-    d = parse_ids_to_dic(all_ids)
-    df = fill_dataframe(d,id,title,abstract,authors,publicationDate,year_only)
+    print(f'Parsing {number_of_pages} documents, saving is {save}, \ntranslating to rus is {tranlsate_to_rus}')
+    articles = await parseArctilcesNumbers(number_of_pages, query)
+    res = await parse_ids_to_dic(articles)
+    df = fill_dataframe(res,id,title,abstract,authors,publicationDate,year_only)
     if tranlsate_to_rus:
         trans = Translator()
         if title:
@@ -111,6 +144,10 @@ def parse(number_of_pages=1,
                 continue
 
     return df
+pd.set_option('display.max_columns', 500)
 
-df=parse(5,save=False)
+
+start_time = time.time()
+df=asyncio.run(parse(5,save=False))
 ic(df)
+print("--- %s seconds ---" % (time.time() - start_time))
